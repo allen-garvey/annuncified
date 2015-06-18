bc6302b4c348215049d0dcf04d14f09992a12547
Allen Garvey <allengarvey@hotmail.com>
2015-06-18 15:49:05 -0400

    modify main menu to use listactivity instead of custom layout

--- app/src/main/java/com/allengarvey/annuncified/MainMenu.java
+++ app/src/main/java/com/allengarvey/annuncified/MainMenu.java
@@ -1,26 +1,26 @@
 package com.allengarvey.annuncified;
 
+import android.app.ListActivity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-import java.util.List;
-
-
-public class MainMenu extends ActionBarActivity{
+public class MainMenu extends ListActivity{
+    private String[] menuList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu);
+        menuList = getResources().getStringArray(R.array.main_menu);
+        setListAdapter(new ArrayAdapter<>(this, R.layout.main_list_layout, R.id.listItem, menuList));
         init();
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -34,9 +34,6 @@ public class MainMenu extends ActionBarActivity{
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -45,22 +42,19 @@ public class MainMenu extends ActionBarActivity{
         NotifyUtil.setSMSReceiverState(this, NotifyUtil.getSMSReceiverStatePreferences(this));
     }
 
-
-
-
-    public void settingsButtonAction(View v){
-        startActivity(new Intent(MainMenu.this, SettingsActivity.class));
-
-    }
-
-    public void settingsHelpButtonAction(View v){
-        startActivity(new Intent(MainMenu.this, HelpActivity.class));
-
-    }
-
-    public void settingsSetNotificationsButtonAction(View v){
-        startActivity(new Intent(MainMenu.this, ChangeNotificationsActivity.class));
-
+    @Override
+    protected void onListItemClick(ListView l, View v, int postion, long id){
+        switch(postion){
+            case 0:
+                startActivity(new Intent(MainMenu.this, ChangeNotificationsActivity.class));
+                break;
+            case 1:
+                startActivity(new Intent(MainMenu.this, SettingsActivity.class));
+                break;
+            default:
+                startActivity(new Intent(MainMenu.this, HelpActivity.class));
+                break;
+        }
     }
 
 }
